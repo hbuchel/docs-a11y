157020240120b115d42919a16dcf45461e664bbe
Heather <hbuchel@gmail.com>
2024-04-04 14:33:14 -0400

    update workflow

--- src/utils/accessibilityTest.ts
+++ src/utils/accessibilityTest.ts
@@ -3,6 +3,7 @@ export const accessibilityTest = async (
   config?: Record<string, unknown>
 ): Promise<void> => {
   if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+    console.log('Accessibility tests will run.');
     const axe = await import('@axe-core/react');
     const ReactDOM = await import('react-dom');
     axe.default(App, ReactDOM, 1000, config);
