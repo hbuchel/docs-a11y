f64f65daa4c133502a781175d359162325636b6f
Heather <hbuchel@gmail.com>
2024-04-05 13:30:27 -0400

    update workflow

---
+++ .github/workflows/scripts/check_for_changed_pages.js
@@ -0,0 +1,23 @@
+module.exports = {
+  getChangedFiles: ({ github, context, core }) => {
+    const {
+      issue: { number: issue_number },
+      repo: { owner, repo }
+    } = context;
+
+    // Use the Github API to query for the list of files from the PR
+    return github
+      .paginate(
+        'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
+        { owner, repo, pull_number: issue_number },
+        (response) => response.data.filter((file) => file.status === 'changed')
+      )
+      .then((files) => {
+       
+        console.log('Changed files: ', files);
+
+        // Return the new files count to be used in the github workflow
+        return files.length;
+      });
+  },
+}
