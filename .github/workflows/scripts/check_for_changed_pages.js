47369e68427731f10e0c005c8c1abbda1f88785f
Heather <hbuchel@gmail.com>
2024-04-05 14:30:15 -0400

    update workflow

--- .github/workflows/scripts/check_for_changed_pages.js
+++ .github/workflows/scripts/check_for_changed_pages.js
@@ -32,7 +32,7 @@ module.exports = {
           if(isMDXPage) {
             const path = filename.replace('src/pages', '').replace('/index.mdx', '');
             platforms.forEach((platform) => {
-              pages.push(path.replace('[platform]', platform));
+              pages.push({ path: path.replace('[platform]', platform) });
             })
           }
         })
