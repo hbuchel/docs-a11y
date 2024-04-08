205c9e336d5871de924248c168f4119821672d07
Heather <hbuchel@gmail.com>
2024-04-08 12:01:31 -0400

    update workflow

--- .github/workflows/scripts/run_axe.js
+++ .github/workflows/scripts/run_axe.js
@@ -1,9 +1,7 @@
 module.exports = {
   runAxe: (pages) => {
-    const { exec } = require('child_process');
     pages.forEach((page) => {
       console.log('page: ', page);
-      exec(`axe http://localhost:3000/${ page } --exit`);
     })
   }
 };
