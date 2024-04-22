5834e1513751f3788949422dbf18214faa2cfcdf
Heather <hbuchel@gmail.com>
2024-04-22 10:46:49 -0400

    update workflow

--- .github/workflows/scripts/run_axe.js
+++ .github/workflows/scripts/run_axe.js
@@ -5,6 +5,7 @@ module.exports = {
     const xml2js = require('xml2js');
 
     var parser = new xml2js.Parser();
+    console.log('dirname: ', __dirname);
     fs.readFile(__dirname + 'public/sitemap.xml', function(err, data) {
       parser.parseString(data, function(err, result) {
         console.dir(result);
