f089d4cd9ef889099f8e546970a328918c10bb38
Heather <hbuchel@gmail.com>
2024-04-22 10:23:53 -0400

    update workflow

--- .github/workflows/scripts/run_axe.js
+++ .github/workflows/scripts/run_axe.js
@@ -1,12 +1,12 @@
-import { getSitemapUrls } from './tasks/link-checker';
-import { exec } from 'child_process';
-
-export const runAxe = async (pages) => {
-  pages.forEach((page) => {
-    console.log('page: ', page);
-    exec(`axe http://localhost:3000${ page } --exit`, (stdout) => {
-      console.log(stdout);
-    });
-  })
-}
-
+module.exports = {
+  runAxe: (pages) => {
+    const { exec } = require('child_process');
+    const { getSiteMapUrls } = require('./tasks/link-checker'); 
+    pages.forEach((page) => {
+      console.log('page: ', page);
+      exec(`axe http://localhost:3000${ page } --exit`, (stdout) => {
+        console.log(stdout);
+      });
+    })
+  }
+};
