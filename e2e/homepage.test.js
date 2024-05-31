5b719c22372e653c1195b5c5b5bca8b940a58afb
Heather <hbuchel@gmail.com>
2024-05-31 15:31:52 -0400

    udpates

--- e2e/homepage.test.js
+++ e2e/homepage.test.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { AxePuppeteer } = require('@axe-core/puppeteer');
 
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
 describe('home page', () => {
   beforeAll(async () => {
     await page.goto(`http://localhost:3000/`);
@@ -14,7 +16,7 @@ describe('home page', () => {
 
   it('should display the home page with no accessibility violations in dark mode', async () => {
     await page.click('button[title="Dark mode"]');
-    await page.waitForSelector('[data-amplify-color-mode="dark"]');
+    await sleep(300);
     const results = await new AxePuppeteer(page).analyze();
     expect(results.violations).toHaveLength(0);
   });
