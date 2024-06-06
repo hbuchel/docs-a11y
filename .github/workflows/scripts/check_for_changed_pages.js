65cb884e5f1b7700013205365bf156d37b36ed05
Heather <hbuchel@gmail.com>
2024-06-06 16:23:47 -0400

    use async await for changed pages

--- .github/workflows/scripts/check_for_changed_pages.js
+++ .github/workflows/scripts/check_for_changed_pages.js
@@ -1,6 +1,5 @@
 module.exports = {
-  getChangedPages: ({ github, context, buildDir }) => {
-    console.log('buildDir: ', buildDir);
+  getChangedPages: async ({ github, context, buildDir }) => {
     const fs = require('fs');
     const cheerio = require('cheerio');
     
@@ -11,54 +10,55 @@ module.exports = {
       repo: { owner, repo }
     } = context;
 
-    // Use the Github API to query for the list of files from the PR
-    return github
-      .paginate(
-        'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
-        { owner, repo, pull_number: issue_number },
-        (response) => response.data.filter((file) => (file.status === 'modified' || file.status === 'added'))
-      )
-      .then((files) => {
-        const possiblePages = [];
-        const platforms = [
-          'android',
-          'angular',
-          'flutter',
-          'javascript',
-          'nextjs',
-          'react',
-          'react-native',
-          'swift',
-          'vue',
-        ]
-        files.forEach(({filename}) => {
-          const isPage = filename.startsWith('src/pages') && (filename.endsWith('index.mdx') || filename.endsWith('index.tsx'));
-          if(isPage) {
-
-            const path = filename.replace('src/pages', '').replace('/index.mdx', '').replace('/index.tsx', '');
-            if(path.includes('[platform]')) {
-              platforms.forEach((platform) => {
-                possiblePages.push(path.replace('[platform]', platform));
-              })
-            } else {
-              possiblePages.push(path);
-            }
-          }
-        });
-
-        const siteMap = fs.readFileSync(`${buildDir}/sitemap.xml`);
+    const possiblePages = [];
+    const platforms = [
+      'android',
+      'angular',
+      'flutter',
+      'javascript',
+      'nextjs',
+      'react',
+      'react-native',
+      'swift',
+      'vue',
+    ];
     
-        const siteMapParse = cheerio.load(siteMap, {
-          xml: true
-        });
+    const changedFiles = await github.paginate(
+      'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
+      { owner, repo, pull_number: issue_number },
+      (response) => response.data.filter((file) => (file.status === 'modified' || file.status === 'added'))
+    );
 
-        siteMapParse('url').each(function () {
-          urlList.push(siteMapParse(this).find('loc').text());
-        });
+    // Get only the changed files that are pages and build out the 
+    // possiblePages array
+    changedFiles.forEach(({filename}) => {
+      const isPage = filename.startsWith('src/pages') && (filename.endsWith('index.mdx') || filename.endsWith('index.tsx'));
+      if(isPage) {
+        const path = filename.replace('src/pages', '').replace('/index.mdx', '').replace('/index.tsx', '');
+        if(path.includes('[platform]')) {
+          platforms.forEach((platform) => {
+            possiblePages.push(path.replace('[platform]', platform));
+          })
+        } else {
+          possiblePages.push(path);
+        }
+      }
+    });
 
-        const pages = possiblePages.filter((page) => urlList.includes(`https://docs.amplify.aws${page}/`));
-        
-        return pages;
-      });
+    // Get the sitemap and parse for an array of site URLs
+    const siteMap = fs.readFileSync(`${buildDir}/sitemap.xml`);
+  
+    const siteMapParse = cheerio.load(siteMap, {
+      xml: true
+    });
+
+    siteMapParse('url').each(function () {
+      urlList.push(siteMapParse(this).find('loc').text());
+    });
+
+    // Filter the possiblePages for only those that are part of the sitemap
+    const pages = possiblePages.filter((page) => urlList.includes(`https://docs.amplify.aws${page}/`));
+    
+    return pages;
   },
 }
